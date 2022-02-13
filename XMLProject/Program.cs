f0289020a7a6da8dc94a65785dab77cd97e4123b
Tarji <tarjiborders@gmail.com>
2022-02-12 19:19:16 -0700

    first commit

---
+++ XMLProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XMLProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XElement customer = XElement.Load(@"C:\Users\perse\Documents\sample.xml");
+            IEnumerable<XElement> childElements =
+                from elements in customer.Elements()
+                select elements;
+            foreach(XElement element in childElements.Elements())
+            {
+                 
+            }
+    
+
+
+
+        }
+    }
+}
