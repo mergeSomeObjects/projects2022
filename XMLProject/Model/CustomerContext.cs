c240b0dc0d1e5f3a7f371f625cb854152d5d1d4d
Tarji <tarjiborders@gmail.com>
2022-02-13 00:08:45 -0700

    update changes

---
+++ XMLProject/Model/CustomerContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XMLProject;
+using XMLProject.Model;
+
+namespace XMLProject.Model
+{
+    class CustomerContext : ObjectContext
+    {
+
+        private ObjectSet<customer> _customers;
+        private ObjectSet<order> _orders;
+
+        public CustomerContext() : base("name=projectEntities1", "projectEntities1")
+        {
+            _customers = CreateObjectSet<customer>();
+            _orders = CreateObjectSet<order>();
+        }
+
+        public ObjectSet<customer> Customers
+        {
+            get { return _customers; }
+        }
+
+        public ObjectSet<order> Orders
+        {
+            get { return _orders; }
+        }
+
+
+
+    }
+
+
+}
--- XMLProject/Program.cs
+++ XMLProject/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using XMLProject.Model;
 
 namespace XMLProject
 {
@@ -13,17 +14,18 @@ namespace XMLProject
         static void Main(string[] args)
         {
 
+            //this needs to be business layer 
             XElement customer = XElement.Load(@"C:\Users\perse\Documents\sample.xml");
             IEnumerable<XElement> childElements =
                 from elements in customer.Elements()
                 select elements;
+
+
             foreach(XElement element in childElements.Elements())
             {
-                 
+                var result = element.LastNode;
             }
     
-
-
 
         }
     }
