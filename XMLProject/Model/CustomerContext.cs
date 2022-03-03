9d99a69336adaa05bf6daaa95eb6b190a294c68d
Tarji <tarjiborders@gmail.com>
2022-03-02 18:49:47 -0700

    updating database layer

--- XMLProject/Model/CustomerContext.cs
+++ XMLProject/Model/CustomerContext.cs
@@ -15,19 +15,71 @@ namespace XMLProject.Model
     public class CustomerContext : DbContext 
     {
 
-        private Microsoft.EntityFrameworkCore.DbSet<customer> _customers { get; set; }
-        private Microsoft.EntityFrameworkCore.DbSet<order> _orders { get; set; }
+        private Microsoft.EntityFrameworkCore.DbSet<Customer> _customers { get; set; }
+        private Microsoft.EntityFrameworkCore.DbSet<Order> _orders { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=projectEntities1;Trusted_Connection=True;");
         }
 
 
+        public class Customer
+        {
+            public int CustomerID { get; set; }
 
+            public string companyName { get; set; }
+
+            public string contactName { get; set; }
+
+            public string contactTitle { get; set;  }
+
+            public string contactPhone { get; set; }
+
+            public string companyAddress { get; set; }
+
+            public string companyCity { get; set; }
+
+            public string regionCity { get; set;  }
+
+            public int postalCode { get; set; }
+
+            public string companyCountry { get; set; }
+
+            public List<Customer> customers { get; set; }
+        }
+
+        public class Order
+        {
+            public int customerID;
+
+            public int employeeID;
+
+            public string orderDate;
+
+            public string requiredDate;
+
+            public string shippedDate;
+
+            public string shipVia;
+
+            public string freight;
+
+            public string shipName;
+
+            public string shipAddress;
+
+            public string shipCity;
+
+            public string shipRegion;
+
+            public int shipPostalCode;
+
+            public string shipCountry;
+
+            public List<Order> orders { get; set; }
+        }
 
     }
-
-
 }
--- XMLProject/Model/CustomerEntity.cs
+++ XMLProject/Model/CustomerEntity.cs
@@ -12,17 +12,53 @@ namespace XMLProject.Model
 
         public static void AddCustomers(List<Customer> customers)
         {
-            using (var context = new projectEntities1())
+            using (var context = new CustomerContext())
             {
+                Model.CustomerContext.Customer c = new CustomerContext.Customer();
+                c.customers = new List<CustomerContext.Customer>();
 
-               
-               
-                context.SaveChanges();
+                Model.CustomerContext.Order o = new CustomerContext.Order();
+                o.orders = new List<CustomerContext.Order>();
+
+
+                foreach (var item in customers)
+                {
+                    var customer = new CustomerContext.Customer
+                    {
+                        CustomerID = Convert.ToInt32(item.customerID),
+                        companyName = item.companyName,
+                        companyAddress = item.companyAddress,
+                        companyCity = item.companyCity,
+                        companyCountry = item.companyCountry,
+                        contactName = item.contactName,
+                        contactPhone = item.contactPhone,
+                        contactTitle = item.contactTitle,
+                        postalCode = Convert.ToInt32(item.postalCode),
+                        regionCity = item.regionCity
+                      
+
+                    };
+
+                    c.customers.Add(customer);
+
+                    //
+                    var order = new CustomerContext.Order
+                    {
+
+
+                    };
+
+                    o.orders.Add(order);
+                };
+
+
+                // o.orders.Add()
+
+
+
+
             }
-
-
-
         }
-
     }
 }
+
